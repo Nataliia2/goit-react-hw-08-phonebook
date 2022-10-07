ac24170180ef1757530cc051958eb59c2999c988
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-10-07 17:19:10 +0300

    форма

---
+++ src/components/ContactFilter.styles.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+  width: 300px;
+  margin-right: auto;
+  margin-left: auto;
+`;
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { List, Item, Button } from './ContactList.styles';
+
+export const ContactList = ({ items, onRemove }) => {
+    const itemList = items.map(({ id, name, number }) => {
+      return (
+        <Item key={id}>
+          {name}: {number}
+          <Button type="button" onClick={() => onRemove(id)}>
+            Delete
+          </Button>
+        </Item>
+      );
+    });
+  
+    return <List>{itemList}</List>;
+  };
+  
+  ContactList.propTypes = {
+    items: PropTypes.arrayOf(
+      PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      })
+    ),
+    onRemove: PropTypes.func.isRequired,
+  };
