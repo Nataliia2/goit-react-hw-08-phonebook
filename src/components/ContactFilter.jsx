3188ad973cabb9ee89271fc37ca64c5c35bfb6de
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-10-26 20:38:49 +0300

    фильтр правки

--- src/components/ContactFilter.jsx
+++ src/components/ContactFilter.jsx
@@ -1,8 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { setContactFilter } from '../redux/FilterSlice';
+import { getStatusFilter } from 'redux/selector';
 import { Wrapper } from './ContactFilter.styles';
 import { Label, Input } from 'components/Form.style';
 
-export const FilterContact = ({onChange, value}) => {
+export const FilterContact = () => {
+  const value = useSelector(getStatusFilter);
+  const dispatch = useDispatch();
   
+  const onChange = ev => {
+    dispatch(setContactFilter(ev.currentTarget.value));
+
+  };
+
   return (
     <Wrapper>
       <Label>Find contacts by name</Label>
