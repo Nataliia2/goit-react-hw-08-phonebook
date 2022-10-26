183c8a0ff6d229e0eef8728fd1f7ba944f397f36
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-10-26 22:33:51 +0300

    фильтр

--- src/redux/FilterSlice.js
+++ src/redux/FilterSlice.js
@@ -5,9 +5,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const filtersSlice = createSlice({
   name: "filters",
   initialState: { filters: ''},
+  
   reducers: {
     setContactFilter(state, action) {
-      return action.payload;
+      state.filters = action.payload;
     },
   },
 });
