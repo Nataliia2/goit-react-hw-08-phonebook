c3662b6a4fadd98a8872791f1b2a1437995fac61
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-10-27 09:20:09 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,19 @@
 import { useSelector } from 'react-redux';
-import { getContacts, getStatusFilter } from "../redux/selector";
+import { getContacts } from "../redux/selector";
 import {ContactForm} from "./Form";
 import {ContactList} from "./ContactList";
 import { FilterContact } from './ContactFilter';
 import  { Title, SubTitle } from "./App.styles";
 
 
+
 export const App = () => {
   const contacts = useSelector(getContacts);
-  const filters = useSelector(getStatusFilter);
+
  
 
 
-  const getFilter = () => {
-    if (!filters) {
-      return contacts;
-    }
-
-    const normalaizedFilter = filters.toLowerCase();
-
-    return contacts.filter(({ name }) => {
-      const normalaizedName = name.toLowerCase();
-      const result = normalaizedName.includes(normalaizedFilter);
-      return result;
-    });
-  };
+ 
 
   return (
     <>
@@ -34,7 +23,7 @@ export const App = () => {
         <>
           <SubTitle>Contacts</SubTitle>
           <FilterContact />
-          <ContactList items={getFilter()} />
+          <ContactList />
         </>
       )}
     </>
--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,3 +1,17 @@
-export const getContacts = state => state.contacts.contacts;
+export const getContacts = state => state.contacts;
 
-export const getStatusFilter = state => state.filters.filters;
+export const getStatusFilter = state => state.filters;
+
+export  const getFilter = ({contacts, filter}) => {
+    if (!filter) {
+      return contacts;
+    }
+
+    const normalaizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) => {
+      const normalaizedName = name.toLowerCase();
+      const result = normalaizedName.includes(normalaizedFilter);
+      return result;
+    });
+  };
