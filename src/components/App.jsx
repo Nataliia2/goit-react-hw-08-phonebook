d008ea1b3f4aeac5f9bb64b572a5532a57767c06
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-11-05 09:34:19 +0200

    правка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,25 @@
-
+import { useSelector } from 'react-redux';
+import { getContacts } from "../redux/selector";
 import {ContactForm} from "./Form";
 import {ContactList} from "./ContactList";
 import { FilterContact } from './ContactFilter';
 import  { Title, SubTitle } from "./App.styles";
 
 
-export function App() {
+export const App = () => {
+  const contacts = useSelector(getContacts);
+
   return (
     <>
       <Title>Phonebook</Title>
       <ContactForm />
+      {contacts !== 0 && (
         <>
           <SubTitle>Contacts</SubTitle>
           <FilterContact />
           <ContactList />
         </>
+      )}
     </>
   );
 };
