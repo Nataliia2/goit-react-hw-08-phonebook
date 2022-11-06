13788f8ef0b4d2ee3dd324416d89cde88aa92765
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-11-06 17:56:32 +0200

    добавила юзэфект

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,14 +1,18 @@
 
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { getFilter, getError, getIsLoading } from 'redux/selector';
-import { deleteContact } from '../redux/contacts/contactsOpetations';
+import { deleteContact, fetchContact } from '../redux/contacts/contactsOpetations';
 import { List, Item, Button } from './ContactList.styles';
+
 import Loader from './Loder';
 export const ContactList = () => {
   const dispatch = useDispatch();
   const items = useSelector(getFilter);
   const error = useSelector(getError);
   const loading = useSelector(getIsLoading);
+
+  useEffect(() => { dispatch(fetchContact()) }, [dispatch]);
     
   const itemList = items.map(({ id, name, number }) => {
       return (
