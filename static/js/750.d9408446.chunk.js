98c14562e099c2c4f5ac7e14fb4faa9f4b68a8da
Nataliia2 <72099196+Nataliia2@users.noreply.github.com>
2022-11-12 22:30:38 +0000

    Deploying to gh-pages from @ Nataliia2/goit-react-hw-08-phonebook@6ea6035ecb2c29d2692f7e47e9a71d82f55f1855 🚀

--- static/js/750.d9408446.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[750],{8750:function(n,e,o){o.r(e),o.d(e,{default:function(){return u}});var t,r=o(168),c=o(6674).Z.p(t||(t=(0,r.Z)(["\n\n"]))),h=o(184);function u(){return(0,h.jsx)(h.Fragment,{children:(0,h.jsx)(c,{children:"My phonebook"})})}}}]);
-//# sourceMappingURL=750.d9408446.chunk.js.map
